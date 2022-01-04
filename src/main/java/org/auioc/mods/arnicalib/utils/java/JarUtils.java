a4f34081c7bf40a560d25b2c3a2171f0fd919041
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-04 15:37:03 +0800

    feat(utils): log with mod name when get manifest failed

--- src/main/java/org/auioc/mods/arnicalib/utils/java/JarUtils.java
+++ src/main/java/org/auioc/mods/arnicalib/utils/java/JarUtils.java
@@ -21,7 +21,7 @@ public interface JarUtils {
             String path = clazz.getResource(clazz.getSimpleName() + ".class").toString();
             return new Manifest(new URL(path.substring(0, path.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF").openStream()).getMainAttributes();
         } catch (Exception e) {
-            LOGGER.warn(MARKER, "MANIFEST.MF could not be read. If this is a development environment you can ignore this message.");
+            LOGGER.warn(MARKER, "MANIFEST.MF of mod " + clazz.getSimpleName() + " could not be read. If this is a development environment you can ignore this message.");
             throw e;
         }
     }
