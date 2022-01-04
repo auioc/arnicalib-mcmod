29a31b5e4003e0d70c7617fd52ce1ee8281ca9b8
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-04 18:42:46 +0800

    feat: version command i18n

--- src/main/java/org/auioc/mods/arnicalib/server/command/impl/VersionCommand.java
+++ src/main/java/org/auioc/mods/arnicalib/server/command/impl/VersionCommand.java
@@ -1,9 +1,9 @@
 package org.auioc.mods.arnicalib.server.command.impl;
 
-import static org.auioc.mods.arnicalib.utils.game.TextUtils.EmptyText;
-import static org.auioc.mods.arnicalib.utils.game.TextUtils.StringText;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
+import org.auioc.mods.arnicalib.ArnicaLib;
+import org.auioc.mods.arnicalib.utils.game.TextUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.MutableComponent;
@@ -11,17 +11,18 @@ import net.minecraft.network.chat.MutableComponent;
 public class VersionCommand {
 
     public static int getModVersion(CommandContext<CommandSourceStack> ctx, String mainVersion, String fullVersion, String modName) {
-        MutableComponent message = EmptyText();
-        message.append(StringText("[" + modName + "] ").withStyle(ChatFormatting.AQUA));
+        MutableComponent message = TextUtils.EmptyText();
+
+        message.append(TextUtils.StringText("[" + modName + "] ").withStyle(ChatFormatting.AQUA));
 
         if (mainVersion.equals("0") && fullVersion.equals("0")) {
-            message.append(StringText("Could not read the mod version. ").withStyle(ChatFormatting.YELLOW));
-            message.append(StringText("If this is a development environment you can ignore this message.").withStyle(ChatFormatting.GRAY));
+            message.append(TextUtils.I18nText(ArnicaLib.MOD_ID + ".command.version.failure"));
         } else {
-            message.append(StringText("Version: " + mainVersion + " (" + fullVersion + ")"));
+            message.append(TextUtils.I18nText(ArnicaLib.MOD_ID + ".command.version.success", mainVersion, fullVersion));
         }
 
         ctx.getSource().sendSuccess(message, false);
+
         return Command.SINGLE_SUCCESS;
     }
 
