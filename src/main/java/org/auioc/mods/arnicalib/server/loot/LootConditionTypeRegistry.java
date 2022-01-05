fe7a223847e2572fac7c6ce99a9915007a6e9157
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-05 10:29:55 +0800

    feat: loaded mod loot item condition

--- src/main/java/org/auioc/mods/arnicalib/server/loot/LootConditionTypeRegistry.java
+++ src/main/java/org/auioc/mods/arnicalib/server/loot/LootConditionTypeRegistry.java
@@ -1,6 +1,7 @@
 package org.auioc.mods.arnicalib.server.loot;
 
 import org.auioc.mods.arnicalib.Reference;
+import org.auioc.mods.arnicalib.server.loot.predicate.LoadedModCondition;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.storage.loot.Serializer;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
@@ -9,6 +10,8 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
 public class LootConditionTypeRegistry {
 
     public static void init() {}
+
+    public static final LootItemConditionType LOADED_MOD = register("loaded_mod", new LoadedModCondition.SerializerX());
 
     private static LootItemConditionType register(String id, Serializer<? extends LootItemCondition> serializer) {
         return Registry.register(Registry.LOOT_CONDITION_TYPE, Reference.ResourceId(id), new LootItemConditionType(serializer));
---
+++ src/main/java/org/auioc/mods/arnicalib/server/loot/predicate/LoadedModCondition.java
@@ -0,0 +1,41 @@
+package org.auioc.mods.arnicalib.server.loot.predicate;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import org.auioc.mods.arnicalib.server.loot.LootConditionTypeRegistry;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.Serializer;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
+import net.minecraftforge.fml.ModList;
+
+public class LoadedModCondition implements LootItemCondition {
+
+    private final String modId;
+
+    public LoadedModCondition(String modId) {
+        this.modId = modId;
+    }
+
+    @Override
+    public boolean test(LootContext ctx) {
+        return ModList.get().isLoaded(this.modId);
+    }
+
+    @Override
+    public LootItemConditionType getType() {
+        return LootConditionTypeRegistry.LOADED_MOD;
+    }
+
+    public static class SerializerX implements Serializer<LoadedModCondition> {
+        @Override
+        public void serialize(JsonObject json, LoadedModCondition instance, JsonSerializationContext ctx) {}
+
+        @Override
+        public LoadedModCondition deserialize(JsonObject json, JsonDeserializationContext ctx) {
+            return new LoadedModCondition(json.get("loaded_mod").getAsString());
+        }
+    }
+
+}
