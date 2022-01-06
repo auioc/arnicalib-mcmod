d2ed340d799e89b2fc52a3a958b704d24a8332fa
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-06 13:32:22 +0800

    feat(utils): method for picking random element from collection

--- src/main/java/org/auioc/mods/arnicalib/utils/java/RandomUtils.java
+++ src/main/java/org/auioc/mods/arnicalib/utils/java/RandomUtils.java
@@ -1,5 +1,24 @@
 package org.auioc.mods.arnicalib.utils.java;
 
+import java.util.Collection;
+import java.util.Iterator;
+import org.apache.commons.lang3.Validate;
+
 public class RandomUtils extends org.apache.commons.lang3.RandomUtils {
+
+    public static <T> T pickOneFromCollection(Collection<T> collection) {
+        int size = collection.size();
+
+        Validate.isTrue(size > 0, "The collection must be not empty.");
+
+        int target = nextInt(0, size);
+        Iterator<T> iterator = collection.iterator();
+        for (int i = 0; i < target; i++) {
+            iterator.next();
+        }
+        T result = iterator.next();
+
+        return result;
+    }
 
 }
