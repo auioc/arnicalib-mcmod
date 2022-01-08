c8048cba5fd01ca156542468a06f9fdfaa788203
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-08 22:42:18 +0800

    feat(loot): set custom effects loot item function

--- src/main/java/org/auioc/mods/arnicalib/server/loot/LootItemFunctionRegistry.java
+++ src/main/java/org/auioc/mods/arnicalib/server/loot/LootItemFunctionRegistry.java
@@ -1,6 +1,7 @@
 package org.auioc.mods.arnicalib.server.loot;
 
 import org.auioc.mods.arnicalib.Reference;
+import org.auioc.mods.arnicalib.server.loot.function.SetCustomEffectsFunction;
 import org.auioc.mods.arnicalib.server.loot.function.SetRandomPotionFunction;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.storage.loot.Serializer;
@@ -12,6 +13,7 @@ public class LootItemFunctionRegistry {
     public static void init() {}
 
     public static final LootItemFunctionType SET_RANDOM_POTION = register("set_random_potion", new SetRandomPotionFunction.SerializerX());
+    public static final LootItemFunctionType SET_CUSTOM_EFFECTS = register("set_custom_effects", new SetCustomEffectsFunction.SerializerX());
 
     private static LootItemFunctionType register(String id, Serializer<? extends LootItemFunction> serializer) {
         return Registry.register(Registry.LOOT_FUNCTION_TYPE, Reference.ResourceId(id), new LootItemFunctionType(serializer));
---
+++ src/main/java/org/auioc/mods/arnicalib/server/loot/function/SetCustomEffectsFunction.java
@@ -0,0 +1,60 @@
+package org.auioc.mods.arnicalib.server.loot.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import org.auioc.mods.arnicalib.server.loot.LootItemFunctionRegistry;
+import org.auioc.mods.arnicalib.utils.game.EffectUtils;
+import org.auioc.mods.arnicalib.utils.java.Validate;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.functions.LootItemConditionalFunction;
+import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+
+public class SetCustomEffectsFunction extends LootItemConditionalFunction {
+
+    private final List<MobEffectInstance> effects;
+
+    protected SetCustomEffectsFunction(LootItemCondition[] conditions, List<MobEffectInstance> effects) {
+        super(conditions);
+        this.effects = effects;
+    }
+
+    @Override
+    public LootItemFunctionType getType() {
+        return LootItemFunctionRegistry.SET_CUSTOM_EFFECTS;
+    }
+
+    @Override
+    protected ItemStack run(ItemStack stack, LootContext ctx) {
+        return PotionUtils.setCustomEffects(stack, this.effects);
+    }
+
+
+    public static class SerializerX extends Serializer<SetCustomEffectsFunction> {
+
+        public void serialize(JsonObject json, SetCustomEffectsFunction instance, JsonSerializationContext ctx) {}
+
+        public SetCustomEffectsFunction deserialize(JsonObject json, JsonDeserializationContext ctx, LootItemCondition[] conditions) {
+            List<MobEffectInstance> effects = new ArrayList<MobEffectInstance>();
+
+            JsonArray effectsJson = GsonHelper.getAsJsonArray(json, "effects");
+            Validate.isTrue(!effectsJson.isEmpty(), "The mob effect instance list must be not empty");
+            for (JsonElement element : effectsJson) {
+                effects.add(EffectUtils.createInstance(GsonHelper.convertToJsonObject(element, "mobEffectInstance")));
+            }
+
+            return new SetCustomEffectsFunction(conditions, effects);
+        }
+
+    }
+
+}
