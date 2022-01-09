933ab3dca5702a6a510902c8edbed082e5be7579
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-09 17:23:31 +0800

    refactor(mixin): overwrite setQueriedLootTableId instead of injecting
    MixinLootContext

--- src/main/java/org/auioc/mods/arnicalib/mixin/server/MixinLootContext.java
+++ src/main/java/org/auioc/mods/arnicalib/mixin/server/MixinLootContext.java
@@ -1,10 +1,8 @@
 package org.auioc.mods.arnicalib.mixin.server;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootContext;
 
@@ -12,16 +10,8 @@ import net.minecraft.world.level.storage.loot.LootContext;
 public abstract class MixinLootContext {
 
     // @org.spongepowered.asm.mixin.Debug(export = true, print = true)
-    @Inject(
-        method = "Lnet/minecraft/world/level/storage/loot/LootContext;setQueriedLootTableId(Lnet/minecraft/resources/ResourceLocation;)V",
-        at = @At(value = "HEAD"),
-        require = 1,
-        allow = 1,
-        cancellable = true,
-        remap = false
-    )
-    private void setQueriedLootTableId(ResourceLocation queriedLootTableId, CallbackInfo ci) {
-        ci.cancel();
+    @Overwrite(remap = false)
+    public void setQueriedLootTableId(ResourceLocation queriedLootTableId) {
         this.queriedLootTableId = queriedLootTableId;
     }
 
