e8afb9cbe74bda26e53cc896e3ccad69bf51cf3c
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-11 15:14:43 +0800

    refactor(utils): annotate CommandUtils.getPrivateSource method as deprecated

--- src/main/java/org/auioc/mods/arnicalib/utils/game/CommandUtils.java
+++ src/main/java/org/auioc/mods/arnicalib/utils/game/CommandUtils.java
@@ -4,6 +4,7 @@ import static org.auioc.mods.arnicalib.ArnicaLib.i18n;
 import static org.auioc.mods.arnicalib.utils.game.TextUtils.I18nText;
 import java.lang.reflect.Field;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import org.auioc.mods.arnicalib.api.mixin.server.IMixinCommandSourceStack;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 
@@ -14,6 +15,17 @@ public interface CommandUtils {
     SimpleCommandExceptionType NOT_DEDICATED_SERVER_ERROR = new SimpleCommandExceptionType(I18nText(i18n("command.failure.not_dedicated_server")));
     SimpleCommandExceptionType REFLECTION_ERROR = new SimpleCommandExceptionType(I18nText(i18n("command.failure.reflection")));
 
+    /**
+     * @param sourceStack
+     * @return The real command source of the specified {@code CommandSourceStack}
+     * @throws NoSuchFieldException
+     * @throws SecurityException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @since 4.0.0
+     * @deprecated Use {@link IMixinCommandSourceStack} instead {@code ((IMixinCommandSourceStack)stack).getSource()}
+     */
+    @Deprecated(since = "4.1.5")
     static CommandSource getPrivateSource(CommandSourceStack sourceStack) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         Field privateSourceField = CommandSourceStack.class.getDeclaredField("source");
         privateSourceField.setAccessible(true);
