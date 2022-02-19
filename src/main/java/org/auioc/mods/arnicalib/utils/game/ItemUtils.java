15d1752f6547426434321b6b4c6aec6b1209fe79
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-19 21:04:01 +0800

    feat(utils): methods for clearing the specified count of items
    org.auioc.mods.arnicalib.utils.game.ItemUtils.clearItem()

--- src/main/java/org/auioc/mods/arnicalib/utils/game/ItemUtils.java
+++ src/main/java/org/auioc/mods/arnicalib/utils/game/ItemUtils.java
@@ -110,4 +110,51 @@ public interface ItemUtils {
         return countItem(container, (stack) -> true);
     }
 
+
+    static int clearItem(ItemStack stack, Predicate<ItemStack> predicate, int count, boolean required) {
+        if (count > 0 && !stack.isEmpty() && predicate.test(stack)) {
+            int r = 0;
+            if (stack.getCount() < count) {
+                if (required) {
+                    return stack.getCount() - count;
+                }
+                r = stack.getCount();
+            } else {
+                r = count;
+            }
+            stack.shrink(r);
+            return r;
+        }
+        return 0;
+    }
+
+    static int clearItem(Container container, Predicate<ItemStack> predicate, int count, boolean required) {
+        if (count <= 0) {
+            return 0;
+        }
+
+        {
+            int c = countItem(container, predicate);
+            if (required && c < count) {
+                return c - count;
+            }
+        }
+
+        int r = 0;
+        for (int i = 0, l = container.getContainerSize(); i < l; i++) {
+            ItemStack stack = container.getItem(i);
+            if (!stack.isEmpty()) {
+                int c = clearItem(stack, predicate, count - r, false);
+                if (c > 0 && stack.isEmpty()) {
+                    container.setItem(i, ItemStack.EMPTY);
+                }
+                r += c;
+                if (r >= count) {
+                    return r;
+                }
+            }
+        }
+        return r - count;
+    }
+
 }
