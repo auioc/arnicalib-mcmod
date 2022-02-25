8a60e013f720d319a2249423a94f35961ce3f690
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-25 23:37:57 +0800

    docs(utils): getAllRegistryObjects method javadoc
    [skip ci]

--- src/main/java/org/auioc/mods/arnicalib/utils/game/RegistryUtils.java
+++ src/main/java/org/auioc/mods/arnicalib/utils/game/RegistryUtils.java
@@ -7,11 +7,19 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import org.auioc.mods.arnicalib.api.game.registry.IHRegistry;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import net.minecraftforge.registries.RegistryObject;
 
 public class RegistryUtils {
 
+    public static <T extends IForgeRegistryEntry<T>> List<T> getAllRegistryObjects(DeferredRegister<T> deferredRegister) {
+        return deferredRegister.getEntries().stream().map(RegistryObject::get).toList();
+    }
+
+    /**
+     * For special use only, please use {@link org.auioc.mods.arnicalib.utils.game.RegistryUtils#getAllRegistryObjects(DeferredRegister)} instead generally.
+     */
     @SuppressWarnings("unchecked")
     public static <T extends IForgeRegistryEntry<? super T>> List<T> getAllRegistryObjects(Class<? extends IHRegistry> clazz, Class<T> type) {
         List<T> list = new ArrayList<T>();
