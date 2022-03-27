4dcd99ad096fb142e9518d6a8e6dda235d49a450
WakelessSloth56 <wsloth56@foxmail.com>
2022-03-27 22:12:01 +0800

    feat(api): BooleanHolder and IntegerHolder

---
+++ src/main/java/org/auioc/mods/arnicalib/api/java/holder/IntegerHolder.java
@@ -0,0 +1,9 @@
+package org.auioc.mods.arnicalib.api.java.holder;
+
+public class IntegerHolder extends ObjectHolder<Integer> {
+
+    public IntegerHolder(Integer value) {
+        super(value);
+    }
+
+}
