78255dca4ed96f671f2388d135dc012f6a3ca35b
WakelessSloth56 <wsloth56@foxmail.com>
2022-03-29 02:34:30 +0800

    feat(utils): CommandFeedbackHelper prefix

--- src/main/java/org/auioc/mods/arnicalib/utils/game/CommandUtils.java
+++ src/main/java/org/auioc/mods/arnicalib/utils/game/CommandUtils.java
@@ -1,6 +1,7 @@
 package org.auioc.mods.arnicalib.utils.game;
 
 import static org.auioc.mods.arnicalib.ArnicaLib.i18n;
+import static org.auioc.mods.arnicalib.utils.game.TextUtils.EmptyText;
 import static org.auioc.mods.arnicalib.utils.game.TextUtils.I18nText;
 import java.lang.reflect.Field;
 import java.util.List;
@@ -16,7 +17,7 @@ import org.auioc.mods.arnicalib.api.mixin.common.IMixinCommandSourceStack;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -89,26 +90,40 @@ public interface CommandUtils {
 
     public static class CommandFeedbackHelper {
 
+        private final MutableComponent prefix;
         private final Function<String, String> i18n;
 
-        public CommandFeedbackHelper(Function<String, String> i18n) {
+        public CommandFeedbackHelper(MutableComponent prefix, Function<String, String> i18n) {
+            this.prefix = prefix;
             this.i18n = i18n;
         }
 
-        public TranslatableComponent successMessage(String key) {
-            return I18nText(this.i18n.apply("command." + key + ".success"));
+        public CommandFeedbackHelper(Function<String, String> i18n) {
+            this(EmptyText(), i18n);
         }
 
-        public TranslatableComponent successMessage(String key, Object... args) {
-            return I18nText(this.i18n.apply("command." + key + ".success"), args);
+        public MutableComponent createMessage(String key) {
+            return EmptyText().append(this.prefix).append(I18nText(this.i18n.apply(key)));
         }
 
-        public TranslatableComponent failureMessage(String key) {
-            return I18nText(this.i18n.apply("command." + key + ".failure"));
+        public MutableComponent createMessage(String key, Object... args) {
+            return EmptyText().append(this.prefix).append(I18nText(this.i18n.apply(key), args));
         }
 
-        public TranslatableComponent failureMessage(String key, Object... args) {
-            return I18nText(this.i18n.apply("command." + key + ".failure"), args);
+        public MutableComponent successMessage(String key) {
+            return this.createMessage("command." + key + ".success");
+        }
+
+        public MutableComponent successMessage(String key, Object... args) {
+            return this.createMessage("command." + key + ".success", args);
+        }
+
+        public MutableComponent failureMessage(String key) {
+            return this.createMessage("command." + key + ".failure");
+        }
+
+        public MutableComponent failureMessage(String key, Object... args) {
+            return this.createMessage("command." + key + ".failure", args);
         }
 
         public int success(CommandContext<CommandSourceStack> ctx, String key) {
