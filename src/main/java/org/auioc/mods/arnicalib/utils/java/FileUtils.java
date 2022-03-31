97bb827b9ed19e1f1663d7814d68a510a548e3f3
WakelessSloth56 <wsloth56@foxmail.com>
2022-03-31 17:59:37 +0800

    feat(utils): writeStringToFile method using UTF-8 charset

--- src/main/java/org/auioc/mods/arnicalib/utils/java/FileUtils.java
+++ src/main/java/org/auioc/mods/arnicalib/utils/java/FileUtils.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import org.apache.logging.log4j.Marker;
 import org.auioc.mods.arnicalib.utils.LogUtil;
 
@@ -15,9 +16,7 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
     public static File getOrCreateDirectory(String directoryName) throws IOException {
         var file = new File(directoryName);
 
-        if (file.exists()) {
-            return file;
-        }
+        if (file.exists()) return file;
 
         if (file.mkdirs()) {
             LOGGER.warn(MARKER, "Directory \"" + file + "\" does not exist, create");
@@ -29,15 +28,26 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
     public static File getFile(String fileName) throws IOException {
         var file = new File(fileName);
 
-        if (file.getParentFile().exists()) {
-            return file;
-        }
+        if (file.getParentFile().exists()) return file;
 
         if (file.getParentFile().mkdirs()) {
             LOGGER.warn(MARKER, "Parent directory of file \"" + file + "\" does not exist, create");
             return file;
         }
         throw new IOException("Could not create parent directory of file \"" + file + "\"");
+    }
+
+
+    /**
+     * Writes a String to a file creating the file if it does not exist using the UTF-8 charset.
+     *
+     * @param file the file to write
+     * @param data the content to write to the file
+     * @throws IOException in case of an I/O error
+     * @since 5.1.4
+     */
+    public static void writeStringToFile(final File file, final String data) throws IOException {
+        writeStringToFile(file, data, Charset.forName("utf8"), false);
     }
 
     /* ============================================================================================================== */
@@ -50,9 +60,7 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
     public static void writeText(String fileName, String text) throws IOException {
         var file = getFile(fileName);
 
-        if (file.exists()) {
-            LOGGER.warn(MARKER, "File \"" + file + "\" already exists, overwrite");
-        }
+        if (file.exists()) LOGGER.warn(MARKER, "File \"" + file + "\" already exists, overwrite");
 
         final var writer = new BufferedWriter(new FileWriter(file, false));
         writer.write(text);
