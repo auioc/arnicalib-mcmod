4278a091ccb8f0b193e1a23c48f0e8eb7fd09f78
WakelessSloth56 <wsloth56@foxmail.com>
2022-04-24 08:24:14 +0800

    refactor(api): remove HSimpleException

--- src/main/java/org/auioc/mcmod/arnicalib/api/java/exception/HSimpleException.java
+++
@@ -1,9 +0,0 @@
-package org.auioc.mcmod.arnicalib.api.java.exception;
-
-public class HSimpleException extends Exception {
-
-    public HSimpleException(String message) {
-        super(message);
-    }
-
-}
