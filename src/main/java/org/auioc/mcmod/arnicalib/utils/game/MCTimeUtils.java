4f8e3459b3c602a499f236c03f3d1c12a2a54048
lainio24 <lainio24@outlook.com>
2022-06-22 18:36:08 +0800

    feat(utils): mc time constants

--- src/main/java/org/auioc/mcmod/arnicalib/utils/game/MCTimeUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/utils/game/MCTimeUtils.java
@@ -4,12 +4,20 @@ import net.minecraft.world.level.Level;
 
 public interface MCTimeUtils {
 
+    int ticksAtSunrise = 23000;
+    int ticksAtDayBegin = 0;
+    int ticksAtDay = 1000;
+    int ticksAtNoon = 6000;
+    int ticksAtSunset = 12000;
+    int ticksAtNight = 13000;
     int ticksAtMidnight = 18000;
+    int ticksAtDayEnd = 24000;
+
     int ticksPerDay = 24000;
     int ticksPerHour = 1000;
-    double ticksPerMinute = 1000d / 60d;
-    double ticksPerSecond = ticksPerMinute / 60d;
-    double ticksPerMillisecond = ticksPerSecond / 1000d;
+    double ticksPerMinute = 1000D / 60D;
+    double ticksPerSecond = ticksPerMinute / 60D;
+    double ticksPerMillisecond = ticksPerSecond / 1000D;
 
     static long[] getTime(Level level) {
         return new long[] {level.getDayTime(), level.getGameTime(), System.currentTimeMillis()};
