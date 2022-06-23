bd159f287d9f26a9d39ff0eb3369681ce91e8032
WakelessSloth56 <wsloth56@foxmail.com>
2022-06-24 01:28:45 +0800

    refactor(command): load test command using CommandHandlerDynamicLoader

--- src/main/java/org/auioc/mcmod/arnicalib/server/command/AHServerCommands.java
+++ src/main/java/org/auioc/mcmod/arnicalib/server/command/AHServerCommands.java
@@ -1,18 +1,11 @@
 package org.auioc.mcmod.arnicalib.server.command;
 
 import static net.minecraft.commands.Commands.literal;
-import static org.auioc.mcmod.arnicalib.ArnicaLib.LOGGER;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
-import org.apache.logging.log4j.Marker;
 import org.auioc.mcmod.arnicalib.ArnicaLib;
 import org.auioc.mcmod.arnicalib.common.command.impl.VersionCommand;
-import org.auioc.mcmod.arnicalib.utils.LogUtil;
-import com.mojang.brigadier.Command;
+import org.auioc.mcmod.arnicalib.utils.game.CommandHandlerDynamicLoader;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.commands.CommandSourceStack;
 
@@ -20,9 +13,15 @@ public final class AHServerCommands {
 
     public static final CommandNode<CommandSourceStack> NODE = literal(ArnicaLib.MOD_ID).build();
 
+    private static final String TEST_COMMAND_CLASS_NAME = "org.auioc.mcmod.arnicalib.server.command.TestCommandHandler";
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         VersionCommand.addVersionNode(NODE, ArnicaLib.class);
-        NODE.addChild(TestCommand.TEST_NODE);
+        NODE.addChild(
+            literal("test")
+                .executes((ctx) -> CommandHandlerDynamicLoader.run(TEST_COMMAND_CLASS_NAME, ctx))
+                .build()
+        );
 
         getAHNode(dispatcher).addChild(NODE);
     }
@@ -42,36 +41,6 @@ public final class AHServerCommands {
     @Deprecated(since = "5.1.3", forRemoval = true)
     public static CommandNode<CommandSourceStack> getRootNode(CommandDispatcher<CommandSourceStack> dispatcher) {
         return getAHNode(dispatcher);
-    }
-
-    private class TestCommand {
-
-        private static final Marker MARKER = LogUtil.getMarker(TestCommand.class);
-
-        private static final String CLASS_NAME = "org.auioc.mcmod.arnicalib.server.command.TestCommandHandler";
-
-        public static final CommandNode<CommandSourceStack> TEST_NODE = literal("test")
-            .executes((ctx) -> {
-                try {
-                    Class<?> c = Class.forName(CLASS_NAME);
-                    Method m = c.getMethod("run", CommandContext.class);
-                    m.invoke(null, ctx);
-                } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException e) {
-                    rethrow(e, "Could not load test command");
-                } catch (InvocationTargetException e) {
-                    if (e.getTargetException() instanceof CommandSyntaxException) {
-                        throw (CommandSyntaxException) e.getTargetException();
-                    }
-                    rethrow(e, "Test command executor throws an exception that is not a CommandSyntaxException");
-                }
-                return Command.SINGLE_SUCCESS;
-            }).build();
-
-        private static void rethrow(Exception e, String message) {
-            LOGGER.error(MARKER, message, e);
-            throw new RuntimeException(e);
-        }
-
     }
 
 }
