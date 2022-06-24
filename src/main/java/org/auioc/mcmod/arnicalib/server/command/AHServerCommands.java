3f61eab1794c623a8184560c06b9e8ae0ebf62de
WakelessSloth56 <wsloth56@foxmail.com>
2022-06-24 23:37:36 +0800

    feat(command): register test node in dev env only

--- src/main/java/org/auioc/mcmod/arnicalib/server/command/AHServerCommands.java
+++ src/main/java/org/auioc/mcmod/arnicalib/server/command/AHServerCommands.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.auioc.mcmod.arnicalib.ArnicaLib;
 import org.auioc.mcmod.arnicalib.common.command.impl.VersionCommand;
 import org.auioc.mcmod.arnicalib.utils.game.DynamicCommandHandler;
+import org.auioc.mcmod.arnicalib.utils.game.EnvironmentUtils;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.commands.CommandSourceStack;
@@ -15,7 +16,7 @@ public final class AHServerCommands {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         VersionCommand.addVersionNode(NODE, ArnicaLib.class);
-        NODE.addChild(TEST_NODE);
+        if (EnvironmentUtils.isDev()) addTestNode(NODE);
 
         getAHNode(dispatcher).addChild(NODE);
     }
@@ -37,14 +38,18 @@ public final class AHServerCommands {
         return getAHNode(dispatcher);
     }
 
-    private static final CommandNode<CommandSourceStack> TEST_NODE = literal("test")
-        .executes(
-            (ctx) -> DynamicCommandHandler.run(
-                "org.auioc.mcmod.arnicalib.server.command.TestCommandHandler",
-                "run",
-                ctx
-            )
-        )
-        .build();
+    private static void addTestNode(CommandNode<CommandSourceStack> node) {
+        node.addChild(
+            literal("test")
+                .executes(
+                    (ctx) -> DynamicCommandHandler.run(
+                        "org.auioc.mcmod.arnicalib.server.command.TestCommandHandler",
+                        "run",
+                        ctx
+                    )
+                )
+                .build()
+        );
+    }
 
 }
