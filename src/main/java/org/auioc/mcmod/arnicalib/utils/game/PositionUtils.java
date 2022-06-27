057eb93933f081f0eb050194b92a1cba0553a6b6
WakelessSloth56 <wsloth56@foxmail.com>
2022-06-27 23:32:09 +0800

    feat(utils): methods for getting random vec3

--- src/main/java/org/auioc/mcmod/arnicalib/utils/game/PositionUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/utils/game/PositionUtils.java
@@ -3,7 +3,9 @@ package org.auioc.mcmod.arnicalib.utils.game;
 import java.util.Random;
 import org.auioc.mcmod.arnicalib.utils.java.RandomUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 
 public interface PositionUtils {
@@ -22,13 +24,26 @@ public interface PositionUtils {
         return BlockUtils.canStandOnSafely(level.getBlockState(pos));
     }
 
-    static BlockPos random(BlockPos center, int radius, Random random) {
-        return new BlockPos(
+    static Vec3i random(Vec3i center, int radius, Random random) {
+        radius += 1;
+        return new Vec3i(
             center.getX() + RandomUtils.offset(radius, random),
             center.getY() + RandomUtils.offset(radius, random),
             center.getZ() + RandomUtils.offset(radius, random)
         );
+    }
 
+    static Vec3 random(Vec3 center, double radius, Random random) {
+        radius += 1.0D;
+        return new Vec3(
+            center.x + RandomUtils.offset(radius, random),
+            center.y + RandomUtils.offset(radius, random),
+            center.z + RandomUtils.offset(radius, random)
+        );
+    }
+
+    static BlockPos random(BlockPos center, int radius, Random random) {
+        return new BlockPos(random(center, radius, random));
     }
 
 }
