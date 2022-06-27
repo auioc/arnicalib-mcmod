1dcbb602459ca068225e5259477f8392e884ae8e
WakelessSloth56 <wsloth56@foxmail.com>
2022-06-28 02:25:12 +0800

    feat(api): HException

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/java/exception/HException.java
@@ -0,0 +1,17 @@
+package org.auioc.mcmod.arnicalib.api.java.exception;
+
+public class HException extends Exception {
+
+    public HException() {
+        super();
+    }
+
+    public HException(String message) {
+        super(message);
+    }
+
+    public HException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
