892b677b76e3d5d18cf3e84a6513e0f0639ddd5d
WakelessSloth56 <wsloth56@foxmail.com>
2022-07-19 01:17:39 +0800

    fix(utils): fix isDev function potential case sensitivity bug

--- src/main/java/org/auioc/mcmod/arnicalib/utils/game/EnvironmentUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/utils/game/EnvironmentUtils.java
@@ -1,5 +1,6 @@
 package org.auioc.mcmod.arnicalib.utils.game;
 
+import java.util.Locale;
 import cpw.mods.modlauncher.Launcher;
 import cpw.mods.modlauncher.api.IEnvironment;
 
@@ -10,6 +11,7 @@ public interface EnvironmentUtils {
             .environment()
             .getProperty(IEnvironment.Keys.LAUNCHTARGET.get())
             .orElse("missing")
+            .toLowerCase(Locale.ROOT)
             .contains("dev");
     }
 
