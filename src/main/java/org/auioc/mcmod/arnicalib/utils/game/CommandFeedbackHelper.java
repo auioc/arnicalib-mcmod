455505156ef1168032be52d68c87003eddcd7900
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-10 00:38:51 +0800

    refactor(utils): refactor CommandFeedbackHelper

--- src/main/java/org/auioc/mcmod/arnicalib/utils/game/CommandFeedbackHelper.java
+++ src/main/java/org/auioc/mcmod/arnicalib/utils/game/CommandFeedbackHelper.java
@@ -3,12 +3,21 @@ package org.auioc.mcmod.arnicalib.utils.game;
 import static org.auioc.mcmod.arnicalib.utils.game.TextUtils.EmptyText;
 import static org.auioc.mcmod.arnicalib.utils.game.TextUtils.I18nText;
 import java.util.function.Function;
+import javax.annotation.Nullable;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.MutableComponent;
 
 public class CommandFeedbackHelper {
+
+    public static enum MessageType {
+        SUCCESS, SUCCESS_BROADCAST, FAILURE
+    }
+
+    public static final Object[] NO_ARGS = new Object[0];
+    public static final ChatFormatting[] NO_STYLE = new ChatFormatting[0];
 
     private final MutableComponent prefix;
     private final Function<String, String> i18n;
@@ -22,48 +31,110 @@ public class CommandFeedbackHelper {
         this(EmptyText(), i18n);
     }
 
-    public MutableComponent createMessage(String key) {
-        return EmptyText().append(this.prefix).append(I18nText(this.i18n.apply(key)));
+
+    private MutableComponent createMessage(MessageType type, String key, @Nullable String subKey, Object... args) {
+        var sb = new StringBuilder(5);
+        {
+            sb.append("command.");
+            sb.append(key);
+            sb.append((type == MessageType.FAILURE) ? ".failure" : ".success");
+            if (subKey != null) {
+                sb.append(".");
+                sb.append(subKey);
+            }
+        }
+        return EmptyText().append(this.prefix).append(I18nText(this.i18n.apply(sb.toString()), args));
     }
 
-    public MutableComponent createMessage(String key, Object... args) {
-        return EmptyText().append(this.prefix).append(I18nText(this.i18n.apply(key), args));
+    public int sendMessage(CommandContext<CommandSourceStack> ctx, MessageType type, String key, @Nullable String subKey, @Nullable Object[] args, @Nullable ChatFormatting[] styles) {
+        if (args == null) args = NO_ARGS;
+        if (styles == null) styles = NO_STYLE;
+
+        var message = createMessage(type, key, subKey, args).withStyle(styles);
+
+        var source = ctx.getSource();
+        switch (type) {
+            case SUCCESS: {
+                source.sendSuccess(message, false);
+                break;
+            }
+            case SUCCESS_BROADCAST: {
+                source.sendSuccess(message, true);
+                break;
+            }
+            case FAILURE: {
+                source.sendFailure(message);
+                break;
+            }
+        }
+
+        return Command.SINGLE_SUCCESS;
     }
 
-    public MutableComponent successMessage(String key) {
-        return this.createMessage("command." + key + ".success");
+
+    public int sendSuccess(CommandContext<CommandSourceStack> ctx, String key) {
+        return sendMessage(ctx, MessageType.SUCCESS, key, null, NO_ARGS, NO_STYLE);
     }
 
-    public MutableComponent successMessage(String key, Object... args) {
-        return this.createMessage("command." + key + ".success", args);
+    public int sendSuccess(CommandContext<CommandSourceStack> ctx, String key, Object... args) {
+        return sendMessage(ctx, MessageType.SUCCESS, key, null, args, NO_STYLE);
     }
 
-    public MutableComponent failureMessage(String key) {
-        return this.createMessage("command." + key + ".failure");
+    public int sendSuccessWithSubkey(CommandContext<CommandSourceStack> ctx, String key, String subKey) {
+        return sendMessage(ctx, MessageType.SUCCESS, key, subKey, NO_ARGS, NO_STYLE);
     }
 
-    public MutableComponent failureMessage(String key, Object... args) {
-        return this.createMessage("command." + key + ".failure", args);
+    public int sendSuccessWithSubkey(CommandContext<CommandSourceStack> ctx, String key, String subKey, Object... args) {
+        return sendMessage(ctx, MessageType.SUCCESS, key, subKey, args, NO_STYLE);
     }
 
+    public int sendSuccessAndBoardcast(CommandContext<CommandSourceStack> ctx, String key) {
+        return sendMessage(ctx, MessageType.SUCCESS_BROADCAST, key, null, NO_ARGS, NO_STYLE);
+    }
+
+    public int sendSuccessAndBoardcast(CommandContext<CommandSourceStack> ctx, String key, Object... args) {
+        return sendMessage(ctx, MessageType.SUCCESS_BROADCAST, key, null, args, NO_STYLE);
+    }
+
+    public int sendFailure(CommandContext<CommandSourceStack> ctx, String key) {
+        return sendMessage(ctx, MessageType.FAILURE, key, null, NO_ARGS, NO_STYLE);
+    }
+
+    public int sendFailure(CommandContext<CommandSourceStack> ctx, String key, Object... args) {
+        return sendMessage(ctx, MessageType.FAILURE, key, null, args, NO_STYLE);
+    }
+
+    public int sendFailureWithSubkey(CommandContext<CommandSourceStack> ctx, String key, String subKey) {
+        return sendMessage(ctx, MessageType.FAILURE, key, subKey, NO_ARGS, NO_STYLE);
+    }
+
+    public int sendFailureWithSubkey(CommandContext<CommandSourceStack> ctx, String key, String subKey, Object... args) {
+        return sendMessage(ctx, MessageType.FAILURE, key, subKey, args, NO_STYLE);
+    }
+
+    /* ============================================================================================================== */
+    // #region Deprecated
+
+    @Deprecated(since = "5.3.6")
     public int success(CommandContext<CommandSourceStack> ctx, String key) {
-        ctx.getSource().sendSuccess(this.successMessage(key), false);
-        return Command.SINGLE_SUCCESS;
+        return sendSuccess(ctx, key);
     }
 
+    @Deprecated(since = "5.3.6")
     public int success(CommandContext<CommandSourceStack> ctx, String key, Object... args) {
-        ctx.getSource().sendSuccess(this.successMessage(key, args), false);
-        return Command.SINGLE_SUCCESS;
+        return sendSuccess(ctx, key, args);
     }
 
+    @Deprecated(since = "5.3.6")
     public int failure(CommandContext<CommandSourceStack> ctx, String key) {
-        ctx.getSource().sendFailure(this.failureMessage(key));
-        return Command.SINGLE_SUCCESS;
+        return sendFailure(ctx, key);
     }
 
+    @Deprecated(since = "5.3.6")
     public int failure(CommandContext<CommandSourceStack> ctx, String key, Object... args) {
-        ctx.getSource().sendFailure(this.failureMessage(key, args));
-        return Command.SINGLE_SUCCESS;
+        return sendFailure(ctx, key, args);
     }
+
+    // #endregion Deprecated
 
 }
