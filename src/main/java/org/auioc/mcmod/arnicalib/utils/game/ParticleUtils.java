3e7e4ddc58dbaf9fd74ff5e2a65197c22966e914
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-11 01:02:11 +0800

    perf(utils): rework drawParticleLine methods

--- src/main/java/org/auioc/mcmod/arnicalib/utils/game/ParticleUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/utils/game/ParticleUtils.java
@@ -2,27 +2,35 @@ package org.auioc.mcmod.arnicalib.utils.game;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec3;
 
 public class ParticleUtils {
 
     private static final Minecraft MC = Minecraft.getInstance();
 
-    public static void drawLine(double x1, double y1, double z1, double x2, double y2, double z2, double stepLength) {
-        drawLine(new Vec3(x1, y1, z1), new Vec3(x2, y2, z2), stepLength);
+    public static void drawLine(Vec3 startPoint, Vec3 endPoint, double stepLength) {
+        drawLine(startPoint.x, startPoint.y, startPoint.z, endPoint.x, endPoint.y, endPoint.z, stepLength);
     }
 
-    public static void drawLine(Vec3 startPoint, Vec3 endPoint, double stepLength) {
-        Vec3 line = startPoint.vectorTo(endPoint);
+    public static void drawLine(double x1, double y1, double z1, double x2, double y2, double z2, double stepLength) {
+        double diffX = (x2 - x1);
+        double diffY = (y2 - y1);
+        double diffZ = (z2 - z1);
 
-        double lineLength = line.length();
+        double lineLength = Mth.length(diffX, diffY, diffZ);
+
         long pointCount = Math.round(lineLength / stepLength);
 
-        Vec3 unit = line.normalize();
+        double projectedStepLengthX = diffX / pointCount;
+        double projectedStepLengthY = diffY / pointCount;
+        double projectedStepLengthZ = diffZ / pointCount;
 
         for (long i = 0; i < pointCount; i++) {
-            Vec3 n = startPoint.add(unit.scale(i * stepLength));
-            MC.level.addParticle(ParticleTypes.ELECTRIC_SPARK, n.x, n.y, n.z, 0.0D, 0.0D, 0.0D);
+            double x = x1 + projectedStepLengthX * i;
+            double y = y1 + projectedStepLengthY * i;
+            double z = z1 + projectedStepLengthZ * i;
+            MC.level.addParticle(ParticleTypes.ELECTRIC_SPARK, x, y, z, 0.0D, 0.0D, 0.0D);
         }
     }
 
