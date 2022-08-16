249b832df8881d989e70981aef23c020432295a1
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-16 16:47:33 +0800

    feat(utils): methods for concatenating text

--- src/main/java/org/auioc/mcmod/arnicalib/utils/game/TextUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/utils/game/TextUtils.java
@@ -1,7 +1,9 @@
 package org.auioc.mcmod.arnicalib.utils.game;
 
+import java.util.List;
 import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.player.Player;
@@ -24,6 +26,24 @@ public interface TextUtils {
 
     static TranslatableComponent translatable(String key, Object... arguments) {
         return new TranslatableComponent(key, arguments);
+    }
+
+
+    static MutableComponent join(List<Component> texts, Component separator) {
+        var r = empty();
+        if (texts.isEmpty()) return r;
+
+        int s = texts.size();
+        for (int i = 0, l = s - 1; i < l; ++i) {
+            r.append(texts.get(i)).append(separator);
+        }
+        r.append(texts.get(s - 1));
+
+        return r;
+    }
+
+    static MutableComponent join(List<Component> texts) {
+        return join(texts, literal(", "));
     }
 
 
