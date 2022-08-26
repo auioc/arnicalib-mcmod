cdb129317a905e710b575506c4d69ff5d0701de1
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-26 11:10:52 +0800

    feat(utils): playerSelf command handlers

---
+++ src/main/java/org/auioc/mcmod/arnicalib/utils/game/command/CommandHandlers.java
@@ -0,0 +1,38 @@
+package org.auioc.mcmod.arnicalib.utils.game.command;
+
+import org.auioc.mcmod.arnicalib.api.game.command.BiCommandAction;
+import org.auioc.mcmod.arnicalib.api.game.command.CommandAction;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+
+public class CommandHandlers {
+
+    public static int playerSelf(CommandContext<CommandSourceStack> ctx, CommandAction.SingleSuccess<ServerPlayer> action) throws CommandSyntaxException {
+        action.accept(ctx.getSource().getPlayerOrException());
+        return Command.SINGLE_SUCCESS;
+    }
+
+    public static int playerSelfWithMainHand(CommandContext<CommandSourceStack> ctx, BiCommandAction.SingleSuccess<ServerPlayer, ItemStack> action) throws CommandSyntaxException {
+        return playerSelf(ctx, (player) -> {
+            if (!player.getMainHandItem().isEmpty()) action.accept(player, player.getMainHandItem());
+        });
+    }
+
+
+    public static class MultiSuccess {
+
+        public static int playerSelf(CommandContext<CommandSourceStack> ctx, CommandAction.MultiSuccess<ServerPlayer> action) throws CommandSyntaxException {
+            return action.applyAsInt(ctx.getSource().getPlayerOrException());
+        }
+
+        public static int playerSelfWithMainHand(CommandContext<CommandSourceStack> ctx, BiCommandAction.MultiSuccess<ServerPlayer, ItemStack> action) throws CommandSyntaxException {
+            return playerSelf(ctx, (player) -> (player.getMainHandItem().isEmpty()) ? 0 : action.applyAsInt(player, player.getMainHandItem()));
+        }
+
+    }
+
+}
