7867177f946481701d16d7d676428be96c576d6d
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-17 00:40:47 +0800

    feat(mixin): mixin arrow to get,set effects of tipped arrow

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/mixin/common/IMixinArrow.java
@@ -0,0 +1,7 @@
+package org.auioc.mcmod.arnicalib.api.mixin.common;
+
+import org.auioc.mcmod.arnicalib.api.game.entity.ITippedArrow;
+
+public interface IMixinArrow extends ITippedArrow {
+
+}
---
+++ src/main/java/org/auioc/mcmod/arnicalib/mixin/common/MixinArrow.java
@@ -0,0 +1,37 @@
+package org.auioc.mcmod.arnicalib.mixin.common;
+
+import java.util.Set;
+import org.auioc.mcmod.arnicalib.api.mixin.common.IMixinArrow;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.item.alchemy.Potion;
+
+@Mixin(value = Arrow.class)
+public class MixinArrow implements IMixinArrow {
+
+    @Shadow
+    private Potion potion;
+
+    @Shadow
+    @Final
+    private Set<MobEffectInstance> effects;
+
+    @Override
+    public Set<MobEffectInstance> getEffects() {
+        return this.effects;
+    }
+
+    @Override
+    public Potion getPotion() {
+        return this.potion;
+    }
+
+    @Override
+    public void setPotion(Potion potion) {
+        this.potion = potion;
+    }
+
+}
