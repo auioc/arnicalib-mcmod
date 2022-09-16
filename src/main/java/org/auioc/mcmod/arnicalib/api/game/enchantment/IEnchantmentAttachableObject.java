81b871566e7f8a341f9061c51a5f666c9330d31f
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-17 00:35:28 +0800

    feat(api): IEnchantmentAttachableObject

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/game/enchantment/IEnchantmentAttachableObject.java
@@ -0,0 +1,18 @@
+package org.auioc.mcmod.arnicalib.api.game.enchantment;
+
+import java.util.Map;
+import javax.annotation.Nullable;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+public interface IEnchantmentAttachableObject {
+
+    @Nullable
+    default Map<Enchantment, Integer> getEnchantments() {
+        return null;
+    }
+
+    default void addEnchantment(Enchantment ench, int lvl) {
+        throw new UnsupportedOperationException("Adding enchantment is not supported by default");
+    }
+
+}
