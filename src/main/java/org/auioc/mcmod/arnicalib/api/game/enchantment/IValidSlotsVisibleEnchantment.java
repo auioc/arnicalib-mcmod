1f2e7b96536000c55d6b2ca340a67c6af2ea4814
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-17 00:35:41 +0800

    feat(api): IValidSlotsVisibleEnchantment

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/game/enchantment/IValidSlotsVisibleEnchantment.java
@@ -0,0 +1,16 @@
+package org.auioc.mcmod.arnicalib.api.game.enchantment;
+
+import org.apache.commons.lang3.ArrayUtils;
+import net.minecraft.world.entity.EquipmentSlot;
+
+public interface IValidSlotsVisibleEnchantment {
+
+    default EquipmentSlot[] getValidSlots() {
+        return new EquipmentSlot[] {};
+    };
+
+    default boolean isValidSlot(EquipmentSlot slot) {
+        return ArrayUtils.contains(getValidSlots(), slot);
+    }
+
+}
