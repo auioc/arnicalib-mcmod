b8f485b7b6c60a61d79e93f6ab8dec8e73c96497
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-17 23:37:00 +0800

    feat(api): LazyObjectHolder

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/java/holder/LazyObjectHolder.java
@@ -0,0 +1,30 @@
+package org.auioc.mcmod.arnicalib.api.java.holder;
+
+import java.util.function.Supplier;
+import javax.annotation.Nullable;
+
+public class LazyObjectHolder<T> implements IObjectHolder<T> {
+
+    private final Supplier<T> supplier;
+    @Nullable
+    private T resolved;
+
+    public LazyObjectHolder(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    @Nullable
+    public T get() {
+        if (this.resolved == null) {
+            this.resolved = this.supplier.get();
+        }
+        return this.resolved;
+    }
+
+    @Override
+    public void set(T value) {
+        throw new UnsupportedOperationException();
+    }
+
+}
