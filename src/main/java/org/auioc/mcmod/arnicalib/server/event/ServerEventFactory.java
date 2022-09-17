5e008fddb8c15cc8b6cc1f8e2f5b933ea7e98c02
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-17 21:34:45 +0800

    feat(event): PiglinStanceEvent

--- src/main/java/org/auioc/mcmod/arnicalib/server/event/ServerEventFactory.java
+++ src/main/java/org/auioc/mcmod/arnicalib/server/event/ServerEventFactory.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import java.util.function.Function;
 import org.apache.logging.log4j.Marker;
 import org.auioc.mcmod.arnicalib.server.event.impl.LivingEatAddEffectEvent;
+import org.auioc.mcmod.arnicalib.server.event.impl.PiglinStanceEvent;
 import org.auioc.mcmod.arnicalib.server.event.impl.ServerLoginEvent;
 import org.auioc.mcmod.arnicalib.server.event.impl.ServerPlayerSendMessageEvent;
 import org.auioc.mcmod.arnicalib.server.event.impl.SetEyeOfEnderSurvivableEvent;
@@ -28,13 +29,13 @@ import net.minecraftforge.eventbus.api.IEventBus;
 public final class ServerEventFactory {
 
     private static final Marker MARKER = LogUtil.getMarker("ServerHooks");
-    private static final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+    private static final IEventBus BUS = MinecraftForge.EVENT_BUS;
 
     // Return true if the event was Cancelable cancelled
 
     public static boolean fireServerLoginEvent(final ClientIntentionPacket packet, final Connection manager) {
         ServerLoginEvent event = new ServerLoginEvent(packet, manager);
-        boolean cancelled = forgeEventBus.post(event);
+        boolean cancelled = BUS.post(event);
         if (cancelled) {
             TextComponent message = new TextComponent(event.getMessage());
             manager.send(new ClientboundLoginDisconnectPacket(message));
@@ -49,12 +50,12 @@ public final class ServerEventFactory {
     }
 
     public static boolean fireServerPlayerSendMessageEvent(ServerPlayer player, Component message, ChatType type, UUID uuid) {
-        return forgeEventBus.post(new ServerPlayerSendMessageEvent(player, message, type, uuid));
+        return BUS.post(new ServerPlayerSendMessageEvent(player, message, type, uuid));
     }
 
     public static List<MobEffectInstance> fireLivingEatAddEffectEvent(LivingEntity entity, ItemStack food, List<MobEffectInstance> effects) {
         LivingEatAddEffectEvent event = new LivingEatAddEffectEvent(entity, food, effects);
-        if (forgeEventBus.post(event)) {
+        if (BUS.post(event)) {
             event.getEffects().clear();
         }
         return event.getEffects();
@@ -62,8 +63,14 @@ public final class ServerEventFactory {
 
     public static Function<Random, Boolean> fireSetEyeOfEnderSurvivableEvent(ServerPlayer player, EyeOfEnder eye) {
         SetEyeOfEnderSurvivableEvent event = new SetEyeOfEnderSurvivableEvent(player, eye);
-        forgeEventBus.post(event);
+        BUS.post(event);
         return event.getSurvivable();
+    }
+
+    public static PiglinStanceEvent.Stance firePiglinStanceEvent(LivingEntity target) {
+        var event = new PiglinStanceEvent(target);
+        BUS.post(event);
+        return event.getStance();
     }
 
 }
