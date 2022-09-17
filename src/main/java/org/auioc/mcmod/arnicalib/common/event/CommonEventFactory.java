34d46feb053a1c3ae36f5f9cec22c68226428b82
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-17 22:21:54 +0800

    feat(event): LivingEatEvent

--- src/main/java/org/auioc/mcmod/arnicalib/common/event/CommonEventFactory.java
+++ src/main/java/org/auioc/mcmod/arnicalib/common/event/CommonEventFactory.java
@@ -1,10 +1,13 @@
 package org.auioc.mcmod.arnicalib.common.event;
 
 import org.auioc.mcmod.arnicalib.common.event.impl.ItemInventoryTickEvent;
+import org.auioc.mcmod.arnicalib.common.event.impl.LivingEatEvent;
 import org.auioc.mcmod.arnicalib.common.event.impl.PistonCheckPushableEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -13,19 +16,25 @@ import net.minecraftforge.eventbus.api.IEventBus;
 
 public final class CommonEventFactory {
 
-    private static final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+    private static final IEventBus BUS = MinecraftForge.EVENT_BUS;
 
     @Deprecated(since = "3.1.1")
     public static boolean postPistonAddBlockLineEvent(BlockState blockState, Level level, BlockPos blockPos, Direction direction) {
-        return forgeEventBus.post(new org.auioc.mcmod.arnicalib.common.event.impl.PistonAddBlockLineEvent(blockState, level, blockPos, direction));
+        return BUS.post(new org.auioc.mcmod.arnicalib.common.event.impl.PistonAddBlockLineEvent(blockState, level, blockPos, direction));
     }
 
     public static boolean firePistonCheckPushableEvent(BlockState blockState, Level level, BlockPos blockPos, Direction pushDirection, boolean p_60209_, Direction p_60210_) {
-        return forgeEventBus.post(new PistonCheckPushableEvent(blockState, level, blockPos, pushDirection, p_60209_, p_60210_));
+        return BUS.post(new PistonCheckPushableEvent(blockState, level, blockPos, pushDirection, p_60209_, p_60210_));
     }
 
     public static boolean onSelectedItemItemInventoryTick(Player player, Level level, ItemStack itemStack, int index) {
-        return forgeEventBus.post(new ItemInventoryTickEvent.Selected(player, level, itemStack, index));
+        return BUS.post(new ItemInventoryTickEvent.Selected(player, level, itemStack, index));
+    }
+
+    public static LivingEatEvent onLivingEat(LivingEntity living, FoodData foodData, ItemStack foodItemStack) {
+        var event = new LivingEatEvent(living, foodData, foodItemStack);
+        BUS.post(event);
+        return event;
     }
 
 }
