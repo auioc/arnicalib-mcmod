c795bcf9758b54787a8c782dac30c37217e23636
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-18 00:04:57 +0800

    feat(mixin): mixin enchantment to get valid slots

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/mixin/common/IMixinEnchantment.java
@@ -0,0 +1,7 @@
+package org.auioc.mcmod.arnicalib.api.mixin.common;
+
+import org.auioc.mcmod.arnicalib.api.game.enchantment.IValidSlotsVisibleEnchantment;
+
+public interface IMixinEnchantment extends IValidSlotsVisibleEnchantment {
+
+}
---
+++ src/main/java/org/auioc/mcmod/arnicalib/mixin/common/MixinEnchantment.java
@@ -0,0 +1,22 @@
+package org.auioc.mcmod.arnicalib.mixin.common;
+
+import org.auioc.mcmod.arnicalib.api.mixin.common.IMixinEnchantment;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+@Mixin(value = Enchantment.class)
+public class MixinEnchantment implements IMixinEnchantment {
+
+    @Shadow
+    @Final
+    private EquipmentSlot[] slots;
+
+    @Override
+    public EquipmentSlot[] getValidSlots() {
+        return this.slots;
+    }
+
+}
