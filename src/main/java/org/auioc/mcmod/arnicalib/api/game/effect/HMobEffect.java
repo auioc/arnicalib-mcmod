6bdf5169dcfe48d538e539e422cc6a88fcb1d64f
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-24 22:23:59 +0800

    feat(api): HMobEffect

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/game/effect/HMobEffect.java
@@ -0,0 +1,12 @@
+package org.auioc.mcmod.arnicalib.api.game.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class HMobEffect extends MobEffect {
+
+    public HMobEffect(MobEffectCategory category, int color) {
+        super(category, color);
+    }
+
+}
