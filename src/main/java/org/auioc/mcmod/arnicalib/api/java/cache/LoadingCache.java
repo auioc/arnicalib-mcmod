41548006c97a33b4f51df5564f78a1d0611f24a9
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-26 00:45:46 +0800

    feat(api): cache

---
+++ src/main/java/org/auioc/mcmod/arnicalib/api/java/cache/LoadingCache.java
@@ -0,0 +1,15 @@
+package org.auioc.mcmod.arnicalib.api.java.cache;
+
+import javax.annotation.Nonnull;
+
+public interface LoadingCache<K, V> extends Cache<K, V> {
+
+    @Nonnull
+    V load(K key);
+
+    @Override
+    default void put(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+}
