d36be34eb6351d9ccf867dea439e123d0c845d55
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-28 01:40:57 +0800

    refactor(next): remove TabVanillaHiddenItems

--- src/main/java/org/auioc/mcmod/arnicalib/Initialization.java
+++ src/main/java/org/auioc/mcmod/arnicalib/Initialization.java
@@ -2,7 +2,6 @@ package org.auioc.mcmod.arnicalib;
 
 import org.auioc.mcmod.arnicalib.client.config.AHClientConfig;
 import org.auioc.mcmod.arnicalib.client.event.AHClientEventHandler;
-import org.auioc.mcmod.arnicalib.common.itemgroup.AHCreativeModeTabs;
 import org.auioc.mcmod.arnicalib.server.event.AHServerEventHandler;
 import org.auioc.mcmod.arnicalib.server.loot.AHGlobalLootModifiers;
 import org.auioc.mcmod.arnicalib.server.loot.AHLootItemConditions;
@@ -55,7 +54,6 @@ public final class Initialization {
         }
 
         private void forgeSetup() {
-            AHCreativeModeTabs.init();
             forgeEventBus.register(AHServerEventHandler.class);
         }
 
