74b0ffa58689b627fadd8da6ce62686ed466799c
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-28 21:40:53 +0800

    refactor(next): remove PistonCheckPushableEvent

--- src/main/java/org/auioc/mcmod/arnicalib/common/event/AHCommonEventFactory.java
+++ src/main/java/org/auioc/mcmod/arnicalib/common/event/AHCommonEventFactory.java
@@ -1,23 +1,15 @@
 package org.auioc.mcmod.arnicalib.common.event;
 
 import org.auioc.mcmod.arnicalib.common.event.impl.ItemInventoryTickEvent;
-import org.auioc.mcmod.arnicalib.common.event.impl.PistonCheckPushableEvent;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public final class AHCommonEventFactory {
 
     private static final IEventBus BUS = MinecraftForge.EVENT_BUS;
-
-    public static boolean onPistonCheckPushable(BlockState blockState, Level level, BlockPos blockPos, Direction pushDirection, boolean p_60209_, Direction p_60210_) {
-        return BUS.post(new PistonCheckPushableEvent(blockState, level, blockPos, pushDirection, p_60209_, p_60210_));
-    }
 
     public static boolean onSelectedItemItemInventoryTick(Player player, Level level, ItemStack itemStack, int index) {
         return BUS.post(new ItemInventoryTickEvent.Selected(player, level, itemStack, index));
