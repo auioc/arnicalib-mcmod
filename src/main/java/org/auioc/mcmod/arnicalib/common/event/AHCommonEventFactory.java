8ed1400673ee6148209e71ff0536ebbd93b71c89
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-28 21:33:53 +0800

    refactor(next): remove LivingEatEvent

--- src/main/java/org/auioc/mcmod/arnicalib/common/event/AHCommonEventFactory.java
+++ src/main/java/org/auioc/mcmod/arnicalib/common/event/AHCommonEventFactory.java
@@ -1,13 +1,10 @@
 package org.auioc.mcmod.arnicalib.common.event;
 
 import org.auioc.mcmod.arnicalib.common.event.impl.ItemInventoryTickEvent;
-import org.auioc.mcmod.arnicalib.common.event.impl.LivingEatEvent;
 import org.auioc.mcmod.arnicalib.common.event.impl.PistonCheckPushableEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -29,12 +26,6 @@ public final class AHCommonEventFactory {
 
     public static boolean onSelectedItemItemInventoryTick(Player player, Level level, ItemStack itemStack, int index) {
         return BUS.post(new ItemInventoryTickEvent.Selected(player, level, itemStack, index));
-    }
-
-    public static LivingEatEvent onLivingEat(LivingEntity living, FoodData foodData, ItemStack foodItemStack) {
-        var event = new LivingEatEvent(living, foodData, foodItemStack);
-        BUS.post(event);
-        return event;
     }
 
 }
