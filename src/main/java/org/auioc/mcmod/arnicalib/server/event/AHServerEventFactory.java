5fc3963e9031b563d007868c4bdf8088feee7b22
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-28 22:16:51 +0800

    refactor(next): remove LivingEatAddEffectEvent

--- src/main/java/org/auioc/mcmod/arnicalib/server/event/AHServerEventFactory.java
+++ src/main/java/org/auioc/mcmod/arnicalib/server/event/AHServerEventFactory.java
@@ -1,23 +1,18 @@
 package org.auioc.mcmod.arnicalib.server.event;
 
 import static org.auioc.mcmod.arnicalib.ArnicaLib.LOGGER;
-import java.util.List;
 import java.util.Random;
 import java.util.function.Function;
 import org.apache.logging.log4j.Marker;
 import org.auioc.mcmod.arnicalib.base.log.LogUtil;
 import org.auioc.mcmod.arnicalib.game.chat.TextUtils;
 import org.auioc.mcmod.arnicalib.server.event.impl.EyeOfEnderSurvivableEvent;
-import org.auioc.mcmod.arnicalib.server.event.impl.LivingEatAddEffectEvent;
 import org.auioc.mcmod.arnicalib.server.event.impl.ServerLoginEvent;
 import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
 import net.minecraft.network.protocol.login.ClientboundLoginDisconnectPacket;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.EyeOfEnder;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 
@@ -42,14 +37,6 @@ public final class AHServerEventFactory {
             return true;
         }
         return false;
-    }
-
-    public static List<MobEffectInstance> onLivingEatAddEffect(LivingEntity entity, ItemStack food, List<MobEffectInstance> effects) {
-        LivingEatAddEffectEvent event = new LivingEatAddEffectEvent(entity, food, effects);
-        if (BUS.post(event)) {
-            event.getEffects().clear();
-        }
-        return event.getEffects();
     }
 
     public static Function<Random, Boolean> onEyeOfEnderSetSurvivable(ServerPlayer player, EyeOfEnder eye) {
