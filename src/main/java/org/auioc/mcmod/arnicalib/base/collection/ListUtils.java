377de770dc35b266cc9804012ab8207574b3ed41
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-11 16:56:41 +0800

    feat(base.collection): methods for inserting an element at specified position in list
    [skip ci]

--- src/main/java/org/auioc/mcmod/arnicalib/base/collection/ListUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/base/collection/ListUtils.java
@@ -12,6 +12,7 @@ public class ListUtils {
         return list.get(list.size() - 1);
     }
 
+
     public static <T> int indexOf(List<T> list, Predicate<T> predicate, int ordinal) {
         for (int i = 0, l = list.size(); i < l; ++i) {
             if (predicate.test(list.get(i))) {
@@ -45,6 +46,24 @@ public class ListUtils {
             .range(0, list.size())
             .filter((i) -> predicate.test(list.get(i)))
             .toArray();
+    }
+
+
+    public static <T> boolean add(List<T> list, int index, T element) {
+        try {
+            list.add(index, element);
+            return true;
+        } catch (IndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    public static <T> boolean addBefore(List<T> list, Predicate<T> refPredicate, T element) {
+        return add(list, indexOf(list, refPredicate), element);
+    }
+
+    public static <T> boolean addAfter(List<T> list, Predicate<T> refPredicate, T element) {
+        return add(list, indexOf(list, refPredicate) + 1, element);
     }
 
 }
