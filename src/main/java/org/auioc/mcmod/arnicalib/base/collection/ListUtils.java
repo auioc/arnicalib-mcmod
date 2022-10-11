3c5e67cc3821e42fc9dad12dc02fe2b6f1b0b6b5
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-11 16:29:48 +0800

    feat(base.collection): method for finding all indexes of list items that match a predicate
    [skip ci]

--- src/main/java/org/auioc/mcmod/arnicalib/base/collection/ListUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/base/collection/ListUtils.java
@@ -2,6 +2,7 @@ package org.auioc.mcmod.arnicalib.base.collection;
 
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 import org.apache.commons.lang3.Validate;
 
 public class ListUtils {
@@ -27,6 +28,13 @@ public class ListUtils {
             }
         }
         return -1;
+    }
+
+    public <T> int[] allIndexesOf(List<T> list, Predicate<T> predicate) {
+        return IntStream
+            .range(0, list.size())
+            .filter((i) -> predicate.test(list.get(i)))
+            .toArray();
     }
 
 }
