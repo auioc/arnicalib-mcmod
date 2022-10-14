6fcd6d4a0d40220cae79ad222d4182fd6cc25ed0
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-14 10:09:03 +0800

    feat(game.mod): backward compatible environment utils
    [skip ci]

---
+++ src/main/java/org/auioc/mcmod/arnicalib/game/cpw/EnvironmentUtils.java
@@ -0,0 +1,12 @@
+package org.auioc.mcmod.arnicalib.game.cpw;
+
+@Deprecated(since = "5.6.2", forRemoval = true)
+public class EnvironmentUtils {
+
+    public static final boolean IS_DEV = org.auioc.mcmod.arnicalib.game.mod.EnvironmentUtils.IS_DEV;
+
+    public static boolean isDev() {
+        return org.auioc.mcmod.arnicalib.game.mod.EnvironmentUtils.isDev();
+    }
+
+}
