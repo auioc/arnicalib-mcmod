03ee7349c89574fc41f15c596951f7c345cff674
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-26 22:16:49 +0800

    feat(mod.mixin): impl ClientPermissionsChangedEvent

---
+++ src/main/java/org/auioc/mcmod/arnicalib/mod/client/event/AHClientEventFactory.java
@@ -0,0 +1,19 @@
+package org.auioc.mcmod.arnicalib.mod.client.event;
+
+import org.auioc.mcmod.arnicalib.game.event.client.ClientPermissionsChangedEvent;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+@OnlyIn(Dist.CLIENT)
+public class AHClientEventFactory {
+
+    private static final IEventBus BUS = MinecraftForge.EVENT_BUS;
+
+    public static void onPermissionChanged(LocalPlayer player, int oldLevel, int newLevel) {
+        BUS.post(new ClientPermissionsChangedEvent(player, oldLevel, newLevel));
+    }
+
+}
---
+++ src/main/java/org/auioc/mcmod/arnicalib/mod/mixin/client/MixinLocalPlayer.java
@@ -0,0 +1,27 @@
+package org.auioc.mcmod.arnicalib.mod.mixin.client;
+
+import org.auioc.mcmod.arnicalib.mod.client.event.AHClientEventFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.player.LocalPlayer;
+
+@Mixin(value = LocalPlayer.class)
+public class MixinLocalPlayer {
+
+    @Shadow
+    private int permissionLevel;
+
+    @Inject(
+        method = "Lnet/minecraft/client/player/LocalPlayer;setPermissionLevel(I)V",
+        at = @At(value = "HEAD"),
+        require = 1,
+        allow = 1
+    )
+    private void setPermissionLevel(int p_108649_, CallbackInfo ci) {
+        AHClientEventFactory.onPermissionChanged(((LocalPlayer) (Object) this), this.permissionLevel, p_108649_);
+    }
+
+}
