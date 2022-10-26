a70826668904492b33083a1070fbb116614adc47
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-26 21:34:31 +0800

    feat(game.widget): remove client widgets
    ClientEsh

--- src/main/java/org/auioc/mcmod/arnicalib/mod/client/ClientInitialization.java
+++ src/main/java/org/auioc/mcmod/arnicalib/mod/client/ClientInitialization.java
@@ -1,14 +1,11 @@
 package org.auioc.mcmod.arnicalib.mod.client;
 
 import org.auioc.mcmod.arnicalib.game.mod.ExtensionPointUtils;
-import org.auioc.mcmod.arnicalib.mod.client.config.AHClientConfig;
 import org.auioc.mcmod.arnicalib.mod.client.event.AHClientEventHandler;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @OnlyIn(Dist.CLIENT)
@@ -25,9 +22,7 @@ public final class ClientInitialization {
     private static final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     private static final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
-    private static void registerConfig() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, AHClientConfig.CONFIG);
-    }
+    private static void registerConfig() {}
 
     private static void modSetup() {}
 
--- src/main/java/org/auioc/mcmod/arnicalib/mod/client/event/AHClientEventHandler.java
+++ src/main/java/org/auioc/mcmod/arnicalib/mod/client/event/AHClientEventHandler.java
@@ -1,34 +1,17 @@
 package org.auioc.mcmod.arnicalib.mod.client.event;
 
 import org.auioc.mcmod.arnicalib.mod.client.command.AHClientCommands;
-import org.auioc.mcmod.arnicalib.mod.client.widget.AdvancedItemTooltip;
-import org.auioc.mcmod.arnicalib.mod.client.widget.DebugScreenInformation;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RegisterClientCommandsEvent;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @OnlyIn(Dist.CLIENT)
 public final class AHClientEventHandler {
 
-    private static final Minecraft MC = Minecraft.getInstance();
-
     @SubscribeEvent
     public static void registerCommands(final RegisterClientCommandsEvent event) {
         AHClientCommands.register(event.getDispatcher());
-    }
-
-    @SubscribeEvent
-    public static void onItemTooltip(ItemTooltipEvent event) {
-        AdvancedItemTooltip.handle(event);
-    }
-
-    @SubscribeEvent
-    public static void onRenderTextOverlay(final RenderGameOverlayEvent.Text event) {
-        if (MC.options.renderDebug) DebugScreenInformation.handle(event.getLeft(), event.getRight());
     }
 
 }
