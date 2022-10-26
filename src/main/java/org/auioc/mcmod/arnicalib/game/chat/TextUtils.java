db391c86fe830c8921b9917265c15230f61311b7
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-26 22:06:23 +0800

    feat(game.chat): methods for creating copyable text

--- src/main/java/org/auioc/mcmod/arnicalib/game/chat/TextUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/game/chat/TextUtils.java
@@ -1,8 +1,11 @@
 package org.auioc.mcmod.arnicalib.game.chat;
 
 import java.util.List;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 
@@ -24,6 +27,19 @@ public class TextUtils {
 
     public static TranslatableComponent translatable(String key, Object... arguments) {
         return new TranslatableComponent(key, arguments);
+    }
+
+
+    public static Style copyable(Style style, String c) {
+        return style.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, c)).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, translatable("chat.copy.click")));
+    }
+
+    public static MutableComponent copyable(Component c) {
+        return TextUtils.empty().append(c).withStyle((s) -> copyable(s, c.getString()));
+    }
+
+    public static MutableComponent copyable(String c) {
+        return copyable(TextUtils.literal(c));
     }
 
 
