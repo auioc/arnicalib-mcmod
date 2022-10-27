38b0a3c36fbbbe5e8780aaf1651539788d029de5
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-27 22:24:33 +0800

    feat(game.config): methods for defining string list

--- src/main/java/org/auioc/mcmod/arnicalib/game/config/ConfigUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/game/config/ConfigUtils.java
@@ -2,9 +2,23 @@ package org.auioc.mcmod.arnicalib.game.config;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 import net.minecraftforge.common.ForgeConfigSpec.Builder;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class ConfigUtils {
+
+    public static final Joiner LINE_JOINER = Joiner.on("\n");
+    public static final Joiner DOT_JOINER = Joiner.on(".");
+    public static final Splitter DOT_SPLITTER = Splitter.on(".");
+
+    public static List<String> split(String path) {
+        return Lists.newArrayList(DOT_SPLITTER.split(path));
+    }
+
 
     public static void pushWithoutPop(Builder specBuilder, String path, Consumer<Builder> subBuilder) {
         specBuilder.push(path);
@@ -15,6 +29,19 @@ public class ConfigUtils {
         specBuilder.push(path);
         subBuilder.accept(specBuilder);
         specBuilder.pop();
+    }
+
+
+    public static <T> ConfigValue<List<? extends T>> defineList(Builder specBuilder, String path, Predicate<Object> elementValidator) {
+        return specBuilder.defineListAllowEmpty(split(path), List::of, elementValidator);
+    }
+
+    public static ConfigValue<List<? extends String>> defineStringList(Builder specBuilder, String path) {
+        return defineList(specBuilder, path, (o) -> o instanceof String);
+    }
+
+    public static ConfigValue<List<? extends String>> defineStringList(Builder specBuilder, String path, List<String> allowedValues) {
+        return defineList(specBuilder, path, (o) -> o instanceof String && allowedValues.contains(o));
     }
 
 
