e549bad78da1f5e5f427570c1295d3a3add740f0
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-27 20:28:07 +0800

    feat(game.entity): method for getting entity facing 8-wind direction

--- src/main/java/org/auioc/mcmod/arnicalib/game/entity/EntityUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/game/entity/EntityUtils.java
@@ -1,8 +1,11 @@
 package org.auioc.mcmod.arnicalib.game.entity;
 
+import org.auioc.mcmod.arnicalib.game.chat.TextUtils;
 import org.auioc.mcmod.arnicalib.game.world.LevelUtils;
 import net.minecraft.core.Vec3i;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
@@ -34,6 +37,12 @@ public class EntityUtils {
 
     public static double calcSpeed(Entity entity) {
         return calcVelocity(entity).length();
+    }
+
+    public static Component getFacing8WindDirection(Entity entity) {
+        float yaw = Mth.wrapDegrees(entity.getYRot());
+        int d8 = (int) Math.floor((yaw - 180.0F) / 45.0F + 0.5F) & 7;
+        return TextUtils.translatable("direction.8wind." + d8);
     }
 
 }
