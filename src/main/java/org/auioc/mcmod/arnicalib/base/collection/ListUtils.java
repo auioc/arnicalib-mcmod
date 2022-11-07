94863cb84099b733f54ea5b5cde129742d27ac0b
WakelessSloth56 <wsloth56@foxmail.com>
2022-11-08 00:00:32 +0800

    feat(base.collection): methods for finding element in list

--- src/main/java/org/auioc/mcmod/arnicalib/base/collection/ListUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/base/collection/ListUtils.java
@@ -1,6 +1,7 @@
 package org.auioc.mcmod.arnicalib.base.collection;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 import org.apache.commons.lang3.Validate;
@@ -10,6 +11,14 @@ public class ListUtils {
     public static <T> T getLast(List<T> list) {
         Validate.notEmpty(list);
         return list.get(list.size() - 1);
+    }
+
+    public static <T> Optional<T> getOptional(List<T> list, int index) {
+        return (index >= 0 && index < list.size()) ? Optional.ofNullable(list.get(index)) : Optional.empty();
+    }
+
+    public static <T> T getOrDefault(List<T> list, int index, T defaultValue) {
+        return (index >= 0 && index < list.size()) ? list.get(index) : defaultValue;
     }
 
 
@@ -49,13 +58,27 @@ public class ListUtils {
     }
 
 
+    public static <T> List<T> findAll(List<T> list, Predicate<T> predicate) {
+        return list.stream().filter(predicate).toList();
+    }
+
+    public static <T> Optional<T> findFirst(List<T> list, Predicate<T> predicate) {
+        return list.stream().filter(predicate).findFirst();
+    }
+
+    public static <T> Optional<T> findFirst(List<T> list, Predicate<T> predicate, int ordinal) {
+        return getOptional(list, indexOf(list, predicate, ordinal));
+    }
+
+    public static <T> Optional<T> findLast(List<T> list, Predicate<T> predicate, int ordinal) {
+        return getOptional(list, lastIndexOf(list, predicate, ordinal));
+    }
+
+
     public static <T> boolean add(List<T> list, int index, T element) {
-        try {
-            list.add(index, element);
-            return true;
-        } catch (IndexOutOfBoundsException e) {
-            return false;
-        }
+        if (index > list.size() || index < 0) return false;
+        list.add(index, element);
+        return true;
     }
 
     public static <T> boolean addBefore(List<T> list, Predicate<T> refPredicate, T element) {
