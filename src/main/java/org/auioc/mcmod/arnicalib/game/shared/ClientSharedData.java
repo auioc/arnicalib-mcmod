4edb8064e3c4358cc5b73d049f1af2923d0b687d
WakelessSloth56 <wsloth56@foxmail.com>
2022-11-18 16:51:50 +0800

    feat(game.shared): client shared data

---
+++ src/main/java/org/auioc/mcmod/arnicalib/game/shared/ClientSharedData.java
@@ -0,0 +1,12 @@
+package org.auioc.mcmod.arnicalib.game.shared;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class ClientSharedData {
+
+    public static double tps = -1.0D;
+    public static double mspt = -1.0D;
+
+}
