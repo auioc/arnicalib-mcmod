3073a508360424d191acd5d91d85bd6710bc0a80
WakelessSloth56 <wsloth56@foxmail.com>
2023-01-16 17:45:57 +0800

    feat(game.nbt): methods for operating nbt with float array

--- src/main/java/org/auioc/mcmod/arnicalib/game/nbt/NbtUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/game/nbt/NbtUtils.java
@@ -2,7 +2,9 @@ package org.auioc.mcmod.arnicalib.game.nbt;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.DoubleTag;
+import net.minecraft.nbt.FloatTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
@@ -19,20 +21,44 @@ public class NbtUtils {
     public static double[] readDoubleArray(ListTag nbt) {
         int size = nbt.size();
         var result = new double[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = nbt.getDouble(i);
-        }
+        for (int i = 0; i < size; i++) result[i] = nbt.getDouble(i);
         return result;
     }
 
     public static ListTag getDoubleListTag(CompoundTag nbt, String key) {
-        return nbt.getList(key, 6);
+        return nbt.getList(key, Tag.TAG_DOUBLE);
     }
 
     public static double[] getDoubleArray(CompoundTag nbt, String key) {
         return readDoubleArray(getDoubleListTag(nbt, key));
     }
 
+    // ====================================================================== //
+
+    public static ListTag writeFloatArray(float... values) {
+        var listTag = new ListTag();
+        for (int i = 0; i < values.length; i++) {
+            listTag.addTag(i, FloatTag.valueOf(values[i]));
+        }
+        return listTag;
+    }
+
+    public static float[] readFloatArray(ListTag nbt) {
+        int size = nbt.size();
+        var result = new float[size];
+        for (int i = 0; i < size; i++) result[i] = nbt.getFloat(i);
+        return result;
+    }
+
+    public static ListTag getFloatListTag(CompoundTag nbt, String key) {
+        return nbt.getList(key, Tag.TAG_FLOAT);
+    }
+
+    public static float[] getFloatArray(CompoundTag nbt, String key) {
+        return readFloatArray(getFloatListTag(nbt, key));
+    }
+
+    // ============================================================================================================== //
 
     public static ListTag writeAABB(AABB aabb) {
         return writeDoubleArray(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ);
@@ -47,6 +73,7 @@ public class NbtUtils {
         return readAABB(getDoubleListTag(nbt, key));
     }
 
+    // ====================================================================== //
 
     public static ListTag writeVec3(Vec3 vec) {
         return writeDoubleArray(vec.x, vec.y, vec.z);
