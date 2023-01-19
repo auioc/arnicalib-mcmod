2c8e52a41fa0e68df01a2082b5c2acec7b038599
WakelessSloth56 <wsloth56@foxmail.com>
2023-01-19 20:51:23 +0800

    feat(game.resource): ResourceUtils

---
+++ src/main/java/org/auioc/mcmod/arnicalib/game/resource/ResourceUtils.java
@@ -0,0 +1,51 @@
+package org.auioc.mcmod.arnicalib.game.resource;
+
+import net.minecraft.ResourceLocationException;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class ResourceUtils {
+
+    private static final Minecraft MC = Minecraft.getInstance();
+
+    public static ResourceLocation modelIdOrElse(String modelId, ResourceLocation other) {
+        try {
+            return modelId.contains("#") ? new ModelResourceLocation(modelId) : new ResourceLocation(modelId);
+        } catch (ResourceLocationException e) {
+            return other;
+        }
+    }
+
+    public static ResourceLocation modelId(String modelId) {
+        return modelIdOrElse(modelId, ModelBakery.MISSING_MODEL_LOCATION);
+    }
+
+    public static boolean isValidModelId(String modelId) {
+        try {
+            if (modelId.contains("#")) {
+                new ModelResourceLocation(modelId);
+            } else {
+                new ResourceLocation(modelId);
+            }
+            return true;
+        } catch (ResourceLocationException e) {
+            return false;
+        }
+    }
+
+    public static BakedModel getBakedModel(String modelId) {
+        return MC.getModelManager().getModel(modelId(modelId));
+    }
+
+    public static BakedModel getBakedModel(ResourceLocation modelId) {
+        return MC.getModelManager().getModel(modelId);
+    }
+
+
+}
