c65891b924bd6d6e2e8f671f1bd6f333a74c83d0
WakelessSloth56 <wsloth56@foxmail.com>
2023-01-20 11:13:13 +0800

    feat(base.phys): PlaneAlignment

---
+++ src/main/java/org/auioc/mcmod/arnicalib/base/phys/PlaneAlignment.java
@@ -0,0 +1,9 @@
+package org.auioc.mcmod.arnicalib.base.phys;
+
+public enum PlaneAlignment {
+
+    TOP_LEFT, TOP_CENTER, TOP_RIGHT,
+    CENTER_LEFT, CENTER, CENTER_RIGHT,
+    BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT
+
+}
