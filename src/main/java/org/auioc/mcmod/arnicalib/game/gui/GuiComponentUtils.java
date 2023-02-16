b957762cd0f73658da8faf6f49407f5b4bbc967c
WakelessSloth56 <wsloth56@foxmail.com>
2023-02-16 21:08:04 +0800

    feat(game.gui): GuiUtils

--- src/main/java/org/auioc/mcmod/arnicalib/game/gui/GuiComponentUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/game/gui/GuiComponentUtils.java
@@ -5,42 +5,35 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.events.GuiEventListener;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ScreenEvent;
 
+/**
+ * @deprecated Use {@link GuiUtils} instead
+ */
+@Deprecated(since = "5.7.3", forRemoval = true)
 @OnlyIn(Dist.CLIENT)
 public class GuiComponentUtils {
 
     public static <T extends GuiEventListener> List<T> getEventListeners(List<GuiEventListener> listeners, Class<T> clazz) {
-        return listeners.stream().filter(clazz::isInstance).map(clazz::cast).toList();
+        return GuiUtils.getEventListeners(listeners, clazz);
     }
 
     public static <T extends GuiEventListener> List<T> getEventListeners(ScreenEvent.InitScreenEvent event, Class<T> clazz) {
-        return getEventListeners(event.getListenersList(), clazz);
+        return GuiUtils.getEventListeners(event, clazz);
     }
-
 
     public static List<Button> getButtons(List<GuiEventListener> listeners, Predicate<Button> predicate) {
-        return getEventListeners(listeners, Button.class).stream().filter(predicate).toList();
+        return GuiUtils.getButtons(listeners, predicate);
     }
 
-
     public static Optional<Button> getButton(List<GuiEventListener> listeners, Predicate<Button> predicate) {
-        return getButtons(listeners, predicate).stream().findFirst();
+        return GuiUtils.getButton(listeners, predicate);
     }
 
     public static Optional<Button> getButton(ScreenEvent.InitScreenEvent event, String messageString) {
-        return getButton(event.getListenersList(), (button) -> {
-            var message = button.getMessage();
-            String _mStr;
-            if (message instanceof TranslatableComponent t) _mStr = t.getKey();
-            else if (message instanceof TextComponent t) _mStr = t.getText();
-            else _mStr = message.getString();
-            return _mStr.equals(messageString);
-        });
+        return GuiUtils.getButton(event, messageString);
     }
 
 }
---
+++ src/main/java/org/auioc/mcmod/arnicalib/game/gui/GuiUtils.java
@@ -0,0 +1,48 @@
+package org.auioc.mcmod.arnicalib.game.gui;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ScreenEvent;
+
+@OnlyIn(Dist.CLIENT)
+public class GuiUtils {
+
+    public static <T extends GuiEventListener> List<T> getEventListeners(List<GuiEventListener> listeners, Class<T> clazz) {
+        return listeners.stream().filter(clazz::isInstance).map(clazz::cast).toList();
+    }
+
+    public static <T extends GuiEventListener> List<T> getEventListeners(ScreenEvent.InitScreenEvent event, Class<T> clazz) {
+        return getEventListeners(event.getListenersList(), clazz);
+    }
+
+    // ====================================================================== //
+
+    public static List<Button> getButtons(List<GuiEventListener> listeners, Predicate<Button> predicate) {
+        return getEventListeners(listeners, Button.class).stream().filter(predicate).toList();
+    }
+
+    public static Optional<Button> getButton(List<GuiEventListener> listeners, Predicate<Button> predicate) {
+        return getButtons(listeners, predicate).stream().findFirst();
+    }
+
+    public static Optional<Button> getButton(ScreenEvent.InitScreenEvent event, String messageString) {
+        return getButton(event.getListenersList(), (button) -> {
+            var message = button.getMessage();
+            String _mStr;
+            if (message instanceof TranslatableComponent t) _mStr = t.getKey();
+            else if (message instanceof TextComponent t) _mStr = t.getText();
+            else _mStr = message.getString();
+            return _mStr.equals(messageString);
+        });
+    }
+
+    // ============================================================================================================== //
+
+}
