57ff05353d9ba73eddd1e5a95f4af129b5a6d9f1
WakelessSloth56 <wsloth56@foxmail.com>
2023-02-16 22:25:00 +0800

    feat: local client test handler

--- src/main/java/org/auioc/mcmod/arnicalib/base/reflection/ReflectionUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/base/reflection/ReflectionUtils.java
@@ -1,31 +1,36 @@
 package org.auioc.mcmod.arnicalib.base.reflection;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 public class ReflectionUtils {
 
-    public static <T> Optional<T> getFieldValue(Object object, Field field, Class<? extends T> type) {
-        Object value;
-        try {
-            value = field.get(object);
-        } catch (IllegalAccessException e) {
-            return Optional.empty();
-        }
-        if (type.isInstance(value)) {
-            return Optional.of(type.cast(value));
-        }
-        return Optional.empty();
-    }
+    // public static <T> Optional<T> getFieldValue(Object object, Field field, Class<? extends T> type) {
+    //     Object value;
+    //     try {
+    //         value = field.get(object);
+    //     } catch (IllegalAccessException e) {
+    //         return Optional.empty();
+    //     }
+    //     if (type.isInstance(value)) {
+    //         return Optional.of(type.cast(value));
+    //     }
+    //     return Optional.empty();
+    // }
 
-    public static <T> Map<String, T> getFieldValues(Object object, Class<? extends T> type) {
-        var map = new HashMap<String, T>();
-        for (var field : object.getClass().getClass().getFields()) {
-            getFieldValue(object, field, type).ifPresent((value) -> map.put(field.getName(), value));
+    // public static <T> Map<String, T> getFieldValues(Object object, Class<? extends T> type) {
+    //     var map = new HashMap<String, T>();
+    //     for (var field : object.getClass().getClass().getFields()) {
+    //         getFieldValue(object, field, type).ifPresent((value) -> map.put(field.getName(), value));
+    //     }
+    //     return map;
+    // }
+
+    public static boolean invokeStatic(String className, String methodName) {
+        try {
+            Class.forName(className).getMethod(methodName).invoke(null);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return map;
+        return false;
     }
 
 }
--- src/main/java/org/auioc/mcmod/arnicalib/mod/client/ClientInitialization.java
+++ src/main/java/org/auioc/mcmod/arnicalib/mod/client/ClientInitialization.java
@@ -1,10 +1,13 @@
 package org.auioc.mcmod.arnicalib.mod.client;
 
+import org.auioc.mcmod.arnicalib.base.reflection.ReflectionUtils;
+import org.auioc.mcmod.arnicalib.game.mod.EnvironmentUtils;
 import org.auioc.mcmod.arnicalib.game.mod.ExtensionPointUtils;
 import org.auioc.mcmod.arnicalib.mod.client.event.AHClientEventHandler;
 import org.auioc.mcmod.arnicalib.mod.client.event.AHClientModEventHandler;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -30,6 +33,15 @@ public final class ClientInitialization {
 
     private static void forgeSetup() {
         forgeEventBus.register(AHClientEventHandler.class);
+        if (EnvironmentUtils.IS_DEV) forgeEventBus.addListener(ClientInitialization::clientTestHandler);
+    }
+
+    // ====================================================================== //
+
+    private static void clientTestHandler(ClientChatReceivedEvent event) {
+        if (event.getMessage().getString().equals("<Dev> .test")) {
+            ReflectionUtils.invokeStatic("org.auioc.mcmod.arnicalib.mod.client.TestHandlerClient", "test");
+        }
     }
 
 }
