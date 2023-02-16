5090d1d4d43b7d868856c63dbebce648929d04ce
WakelessSloth56 <wsloth56@foxmail.com>
2023-02-16 23:13:24 +0800

    feat(game.registry): methods for sorting registry entry map,list by id

--- src/main/java/org/auioc/mcmod/arnicalib/game/registry/RegistryUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/game/registry/RegistryUtils.java
@@ -5,8 +5,12 @@ import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistryEntry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -18,10 +22,38 @@ public class RegistryUtils {
         return deferredRegister.getEntries().stream().map(RegistryObject::get).toList();
     }
 
+    // ============================================================================================================== //
+
+    public static <K extends ForgeRegistryEntry<?>> Comparator<K> comparator() {
+        return new Comparator<K>() {
+            @Override
+            public int compare(K o1, K o2) { return o1.getRegistryName().compareTo(o2.getRegistryName()); }
+        };
+    }
+
+    public static <K extends ForgeRegistryEntry<?>, V> Comparator<Map.Entry<K, V>> mapComparator() {
+        return Map.Entry.comparingByKey(comparator());
+    }
+
+    // ====================================================================== //
+
+    public static <K extends ForgeRegistryEntry<?>, V> LinkedHashMap<K, V> sortMap(Map<K, V> map) {
+        var linkedMap = new LinkedHashMap<K, V>(map.size(), 1.0F);
+        map.entrySet().stream().sorted(mapComparator()).forEach((e) -> linkedMap.put(e.getKey(), e.getValue()));
+        return linkedMap;
+    }
+
+    public static <T extends ForgeRegistryEntry<?>> List<T> sortList(List<T> list) {
+        return list.stream().sorted(comparator()).toList();
+    }
+
+    // ============================================================================================================== //
+
     /**
      * For special use only,
-     * please use {@link org.auioc.mcmod.arnicalib.game.registry.RegistryUtils#getAllRegistryObjects(DeferredRegister)} instead generally.
+     * please use {@link #getAllRegistryObjects(DeferredRegister)} instead generally.
      */
+    @Deprecated
     @SuppressWarnings("unchecked")
     public static <T extends IForgeRegistryEntry<? super T>> List<T> getAllRegistryObjects(Class<?> clazz, Class<T> type) {
         List<T> list = new ArrayList<T>();
