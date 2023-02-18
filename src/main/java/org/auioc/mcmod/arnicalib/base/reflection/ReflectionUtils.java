dbe976e0c4136af798de3cb540d4cbb238560156
WakelessSloth56 <wsloth56@foxmail.com>
2023-02-18 21:59:49 +0800

    feat(base.reflection): ReflectionUtils

--- src/main/java/org/auioc/mcmod/arnicalib/base/reflection/ReflectionUtils.java
+++ src/main/java/org/auioc/mcmod/arnicalib/base/reflection/ReflectionUtils.java
@@ -1,27 +1,90 @@
 package org.auioc.mcmod.arnicalib.base.reflection;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public class ReflectionUtils {
 
-    // public static <T> Optional<T> getFieldValue(Object object, Field field, Class<? extends T> type) {
-    //     Object value;
-    //     try {
-    //         value = field.get(object);
-    //     } catch (IllegalAccessException e) {
-    //         return Optional.empty();
-    //     }
-    //     if (type.isInstance(value)) {
-    //         return Optional.of(type.cast(value));
-    //     }
-    //     return Optional.empty();
-    // }
+    public static <V> Optional<V> getFieldValue(Object object, Field field, Class<? extends V> type) {
+        Object value;
+        try {
+            field.setAccessible(true);
+            value = field.get(object);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+        if (type.isInstance(value)) {
+            return Optional.of(type.cast(value));
+        }
+        return Optional.empty();
+    }
 
-    // public static <T> Map<String, T> getFieldValues(Object object, Class<? extends T> type) {
-    //     var map = new HashMap<String, T>();
-    //     for (var field : object.getClass().getClass().getFields()) {
-    //         getFieldValue(object, field, type).ifPresent((value) -> map.put(field.getName(), value));
-    //     }
-    //     return map;
-    // }
+    // ====================================================================== //
+
+    public static <V> Map<String, V> getFieldValues(Class<?> clazz, Object object, Class<? extends V> type) {
+        var map = new HashMap<String, V>();
+        for (var field : clazz.getFields()) {
+            getFieldValue(object, field, type).ifPresent((value) -> map.put(field.getName(), value));
+        }
+        return map;
+    }
+
+    public static <V> Map<String, V> getFieldValues(Object object, Class<? extends V> type) {
+        return getFieldValues(object.getClass(), object, type);
+    }
+
+    public static <V> Map<String, V> getFieldValues(Class<?> clazz, Class<? extends V> type) {
+        return getFieldValues(clazz, null, type);
+    }
+
+    // ====================================================================== //
+
+    public static List<Class<?>> getClasses(Object object) {
+        Class<?> clazz = (object instanceof Class<?>) ? (Class<?>) object : object.getClass();
+        final var classes = new ArrayList<Class<?>>();
+        do {
+            classes.add(clazz);
+            clazz = clazz.getSuperclass();
+        } while (clazz != Object.class);
+        return classes;
+    }
+
+    // ====================================================================== //
+
+    public static List<Field> getFields(Class<?> clazz) {
+        final var fields = new ArrayList<Field>();
+        try {
+            for (var field : clazz.getDeclaredFields()) {
+                fields.add(field);
+            }
+        } catch (SecurityException e) {
+        }
+        return fields;
+    }
+
+    public static Map<Class<?>, List<Field>> getAllFields(Object object) {
+        var classes = getClasses(object);
+        final var result = new LinkedHashMap<Class<?>, List<Field>>(classes.size(), 1);
+        for (var c : classes) {
+            result.put(c, getFields(c));
+        }
+        return result;
+    }
+
+    public static List<Field> getAllFieldsFlat(Object object) {
+        final var fields = new ArrayList<Field>();
+        for (var c : getClasses(object)) {
+            fields.addAll(getFields(c));
+        }
+        return fields;
+    }
+
+    // ====================================================================== //
 
     public static boolean invokeStatic(String className, String methodName) {
         try {
