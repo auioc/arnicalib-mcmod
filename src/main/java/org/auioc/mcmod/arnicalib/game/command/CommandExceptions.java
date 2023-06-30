f305361902a26f2d2896cecd8d0b84794c66a2d4
WakelessSloth56 <wsloth56@foxmail.com>
2023-06-30 11:19:54 +0800

    feat(porting): remove get_real_source command exception

--- src/main/java/org/auioc/mcmod/arnicalib/game/command/CommandExceptions.java
+++ src/main/java/org/auioc/mcmod/arnicalib/game/command/CommandExceptions.java
@@ -14,7 +14,6 @@ public class CommandExceptions {
     public static final SimpleCommandExceptionType LOGGABLE_INTERNAL_ERROR = simple("command.failure.internal.loggable");
     public static final SimpleCommandExceptionType NOT_SERVER_ERROR = simple("command.failure.not_server");
     public static final SimpleCommandExceptionType NOT_DEDICATED_SERVER_ERROR = simple("command.failure.not_dedicated_server");
-    public static final SimpleCommandExceptionType GET_REAL_SOURCE_REFLECTION_ERROR = simple("command.failure.get_real_source.reflection");
 
     private static SimpleCommandExceptionType simple(String key) {
         return new SimpleCommandExceptionType(Component.translatable(ArnicaLib.i18n("command.failure.internal")));
