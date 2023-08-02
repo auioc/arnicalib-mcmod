276a85ed8e1e3f83986b45209f43006c772ddd15
WakelessSloth56 <wsloth56@foxmail.com>
2023-08-03 02:49:22 +0800

    feat(base.function): ToStringFunction
    [skip ci]
    
    Co-authored-by: lainio24 <lainio24@outlook.com>

--- src/main/java/org/auioc/mcmod/arnicalib/base/function/StringUnaryOperator.java
+++ src/main/java/org/auioc/mcmod/arnicalib/base/function/StringUnaryOperator.java
@@ -5,8 +5,6 @@ import java.util.function.UnaryOperator;
 @FunctionalInterface
 public interface StringUnaryOperator extends UnaryOperator<String> {
 
-    String apply(String s);
-
     @Deprecated(since = "6.0.3", forRemoval = true)
     default String applyAsString(String s) {
         return apply(s);
---
+++ src/main/java/org/auioc/mcmod/arnicalib/base/function/ToStringFunction.java
@@ -0,0 +1,8 @@
+package org.auioc.mcmod.arnicalib.base.function;
+
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface ToStringFunction<T> extends Function<T, String> {
+
+}
