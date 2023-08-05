87c8c3503fc8329db067e57d2b355aa7e8582070
WakelessSloth56 <wsloth56@foxmail.com>
2023-08-05 10:08:46 +0800

    fix(game.entity): remove entity data serializers
    fix #11

--- src/main/java/org/auioc/mcmod/arnicalib/mod/Initialization.java
+++ src/main/java/org/auioc/mcmod/arnicalib/mod/Initialization.java
@@ -2,7 +2,6 @@ package org.auioc.mcmod.arnicalib.mod;
 
 import org.auioc.mcmod.arnicalib.game.mod.ExtensionPointUtils;
 import org.auioc.mcmod.arnicalib.mod.client.ClientInitialization;
-import org.auioc.mcmod.arnicalib.mod.common.entity.AHEntityDataSerializers;
 import org.auioc.mcmod.arnicalib.mod.common.tag.HTags;
 import org.auioc.mcmod.arnicalib.mod.server.event.AHServerEventHandler;
 import org.auioc.mcmod.arnicalib.mod.server.loot.AHGlobalLootModifiers;
@@ -34,7 +33,6 @@ public final class Initialization {
 
     private static void modSetup() {
         modEventBus.register(AHGlobalLootModifiers.class);
-        AHEntityDataSerializers.ENTITY_DATA_SERIALIZERS.register(modEventBus);
         AHGlobalLootModifiers.GLOBAL_LOOT_MODIFIERS.register(modEventBus);
         AHLootItemConditions.LOOT_CONDITION_TYPES.register(modEventBus);
         AHLootItemFunctions.LOOT_FUNCTION_TYPES.register(modEventBus);
